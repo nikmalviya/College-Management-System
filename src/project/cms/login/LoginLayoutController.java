e6dc7c7c66cb28171dd514658d78343114967a5b
Nikhil Malviya <nikhilmalviya1662000@gmail.com>
2018-09-13 20:15:00 +0530

    Added login and main package

--- src/project/cms/login/LoginLayoutController.java
+++ src/project/cms/login/LoginLayoutController.java
@@ -3,30 +3,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package project.cms;
+package project.cms.login;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 
 /**
+ * FXML Controller class
  *
  * @author programmer
  */
-public class MainController implements Initializable {
-    
-    @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+public class LoginLayoutController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
--- src/project/cms/main/Main.java
+++ src/project/cms/main/Main.java
@@ -1,5 +1,5 @@
 
-package project.cms;
+package project.cms.main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -8,7 +8,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
-public class main extends Application {
+public class Main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
