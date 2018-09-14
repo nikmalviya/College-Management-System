b05b8e1a5439a183059ce3770eaa5f885aa3b2e7
Nikhil Malviya <nikhilmalviya1662000@gmail.com>
2018-09-14 14:45:49 +0530

    Modified Main File with login program

--- src/project/cms/main/Main.java
+++ src/project/cms/main/Main.java
@@ -2,9 +2,14 @@
 package project.cms.main;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
@@ -12,11 +17,22 @@ public class Main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-        
-        Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
+        Label username = new Label("User Name");
+        Label password = new Label("Password");
+        TextField usertext = new TextField();
+        PasswordField pass = new PasswordField();
+        GridPane grid = new GridPane();
+        grid.addColumn(0,username,password);
+        grid.addColumn(1,usertext,pass);
+        Button btn = new Button("login");
+        TextField t = new TextField();
+        t.setEditable(false);
+        btn.addEventHandler(MouseEvent.MOUSE_CLICKED, e ->{
+            System.out.println("login clicked");
+            t.setText(usertext.getText()+" "+pass.getText());
+        });
+        VBox pane = new VBox(10,grid,t,btn);
+        stage.setScene(new Scene(pane));
         stage.show();
     }
 
