93e4530a9a7f5fd1c4cf7cf7370910e0c62a778e
Nikhil Malviya <nikhilmalviya1662000@gmail.com>
2018-09-19 22:33:52 +0530

    Designed studentview UI

--- src/project/cms/dashboard/DashBoardController.java
+++ src/project/cms/dashboard/DashBoardController.java
@@ -1,11 +1,13 @@
 package project.cms.dashboard;
 
+import com.jfoenix.controls.JFXButton;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -16,8 +18,12 @@ public class DashBoardController {
     URL location;
     @FXML
     private AnchorPane root;
+    @FXML
+    private JFXButton home;
+    @FXML
+    private JFXButton student;
     public void initialize() throws IOException {
-        AnchorPane pane = FXMLLoader.load(getClass().getResource("/project/cms/students/addstudent/addStudent.fxml"));
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("/project/cms/students/studentsview.fxml"));
         Stage addStudent = new Stage();
         addStudent.setScene(new Scene(pane));
         addStudent.showAndWait();
